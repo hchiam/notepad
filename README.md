ef7624b12ba319dd7772c4eb629601554551d5c2
hchiam <hchiam@users.noreply.github.com>
2021-11-05 22:03:50 -0400

    fix #2 and kinda help with #3: localStorage upon open and upon close

--- README.md
+++ README.md
@@ -12,5 +12,4 @@ yarn deploy
 ## TODO
 
 - #1: if yes, read text file: <https://codepen.io/hchiam/pen/xxLWbop>
-- #2: detect closing tab - see stack overflow
 - #3: add saving upon closing
--- site_files/index.js
+++ site_files/index.js
@@ -1,11 +1,27 @@
-document.querySelector("textarea").focus();
+var textarea = document.querySelector("textarea");
+textarea.focus();
 
 /**
 
 ## TODO
 
 - #1: if yes, read text file: <https://codepen.io/hchiam/pen/xxLWbop>
-- #2: detect closing tab - see stack overflow
 - #3: add saving upon closing
 
  */
+
+var rememberedText = localStorage.getItem("simple-notepad");
+var yes = confirm("Restore previous?");
+textarea.value = yes && rememberedText ? rememberedText : "";
+
+textarea.addEventListener("keyup", function () {
+  localStorage.setItem("simple-notepad", textarea.value);
+});
+
+window.addEventListener("beforeunload", function (e) {
+  localStorage.setItem("simple-notepad", textarea.value);
+  var confirmationMessage = "o/";
+
+  (e || window.event).returnValue = confirmationMessage; //Gecko + IE
+  return confirmationMessage; //Webkit, Safari, Chrome
+});
