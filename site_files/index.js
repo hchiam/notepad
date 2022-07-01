6db960f89119b13118e04337e0cacc9997b5f6c9
hchiam <hchiam@users.noreply.github.com>
2022-07-01 16:50:40 -0600

    save

--- site_files/index.js
+++ site_files/index.js
@@ -12,3 +12,40 @@ textarea.addEventListener("keyup", function () {
 window.addEventListener("beforeunload", function (e) {
   localStorage.setItem("simple-notepad", textarea.value);
 });
+
+document.addEventListener(
+  "keydown",
+  function (e) {
+    if (
+      (e.key === "s" || e.keyCode === 83) &&
+      (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)
+    ) {
+      e.preventDefault();
+      var data = textarea.value;
+      var filename = "notepad.txt";
+      var type = "text/plain;charset=utf-8";
+      download(data, filename, type);
+    }
+  },
+  false
+);
+
+function download(data, filename, type) {
+  var file = new Blob([data], { type: type });
+
+  var isIE10OrLater = window.navigator.msSaveOrOpenBlob;
+  if (isIE10OrLater) {
+    window.navigator.msSaveOrOpenBlob(file, filename);
+  } else {
+    var a = document.createElement("a");
+    var url = URL.createObjectURL(file);
+    a.href = url;
+    a.download = filename;
+    document.body.appendChild(a);
+    a.click();
+    setTimeout(function () {
+      document.body.removeChild(a);
+      window.URL.revokeObjectURL(url);
+    }, 0);
+  }
+}
